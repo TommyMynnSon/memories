04ab75afae94c31259f8600a0eb96c8ec56a3c99
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-20 12:55:40 -0800

    Add connection to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -10,3 +10,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 // MongoDB
+
+const CONNECTION_URL = "mongodb+srv://tommyson:0000@cluster0.c1goa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`)))
+  .catch((error) => console.log(error.message));
