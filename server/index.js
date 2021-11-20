64bb39c312aa194969d97d8db450cd505175e430
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-20 13:03:35 -0800

    Import routes/posts.js to server/index.js

--- server/index.js
+++ server/index.js
@@ -3,14 +3,18 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+// Routers.
+import postRoutes from './routes/posts.js';
+
+// Start new Express application.
 const app = express();
 
+// Middleware.
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-// MongoDB
-
+// MongoDB.
 const CONNECTION_URL = "mongodb+srv://tommyson:0000@cluster0.c1goa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 3000;
 
