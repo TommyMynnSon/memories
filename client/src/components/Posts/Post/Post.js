ca6ccc86c52e3d0a75901db7ba91c4f04c750f24
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-21 15:14:20 -0800

    Import MUI components and icons

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,5 +1,12 @@
+// React
 import React from 'react';
+
+// Material-UI
 import useStyles from './styles';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 const Post = () => {
   const classes = useStyles();
