372144c577cbf0823b6a0b50501b908707a25ef5
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-21 02:04:19 -0800

    Wrap App component with a Provider component

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { Provider, createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
@@ -9,4 +9,9 @@ import App from './App';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
