b1fb557c05eee2c4f6639ee752f8bf3527a48288
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-21 12:32:09 -0800

    Fix import of useDispatch

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
-import { useDispatch, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
