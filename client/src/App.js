66a9bcb8968c7b0ce3487e185c278ac6f1a24601
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-20 17:04:49 -0800

    Import memories.png from images folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import memories from './images/memories.png';
+
 const App = () => {
   return (
     <Container maxidth="lg">
