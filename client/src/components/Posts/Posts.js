23d2736d2af3baf854420fa2d8e8309e7d9e8a39
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-21 15:11:57 -0800

    Implement JSX and Post component rendering for the Posts component

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -18,11 +18,15 @@ const Posts = () => {
   console.log(posts);
 
   return (
-    <>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
-    </>
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   );
 };
 
